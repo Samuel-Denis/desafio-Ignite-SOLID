1831b079c87f55563f0f83bdd2e0e1a926448dd0
Samuel Denis <samueldenisbb@gmail.com>
2021-03-21 18:10:52 -0300

    desafio feito

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdm = this.usersRepository.findById(user_id);
+
+    if (!userIsAdm) {
+      throw new Error("User not found");
+    }
+    if (!userIsAdm.admin) {
+      throw new Error("User is not adm, permission denied");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
